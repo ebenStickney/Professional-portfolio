4c2de74470963838825e504b1834d82521745d7d
ebenStickney <36087626+ebenStickney@users.noreply.github.com>
2018-06-11 14:39:16 -0700

    about update

--- src/components/About.js
+++ src/components/About.js
@@ -4,33 +4,75 @@ import React from 'react';
 
 
 
-const AboutPage = () => (
+class AboutPage extends React.Component {
+  state = {
+    imgUrl: '/images/profile-pic.jpg'
+  };
 
-    <div className="content-container__about">
-      <div className="flex-item__main main1">
-        <h3 className="profile__header">Eben Stickney</h3>
-        <img className='profile__pic' src='/images/profile-pic.jpg' />
+  changePic = () => {
+    const pic = this.state.imgUrl === '/images/profile-pic.jpg' ?
+    '/images/Goat_profile.jpg' : '/images/profile-pic.jpg';
+    this.setState( () => ({imgUrl: pic}))
+  };
+
+  render() {
+    return (
+      <div className="content-container__about">
+        <div className="flex-item__main main1">
+          <div className="profile__split">
+            <h3 className="profile__header">Eben Stickney</h3>
+          </div>
+          <div className="profile__split-2">
+              <section className="icon__section">
+
+                <div className="icon__item">
+                  <a className="icon" href="https://www.linkedin.com/in/eben-stickney-28512417/" target="_blank">
+                    <i className="fab fa-linkedin fa-3x"></i>
+                  </a>
+                </div>
+                  <div className="icon__item">
+                  <a className="icon" href="https://github.com/ebenStickney" target="_blank">
+                    <i className="fab fa-github-square fa-3x"></i>
+                  </a>
+                </div>
+                <div className="icon__item">
+                  <a className="icon" href="https://twitter.com/ebenstickney" target="_blank">
+                    <i className="fab fa-twitter-square fa-3x"></i>
+                  </a>
+                </div>
+              </section>
+              <input type="image" className='profile__pic' src={this.state.imgUrl} alt='Profile Picute' onClick={this.changePic} />
+
+            </div>
+        </div>
+
+
+        <div className="flex-item__main">
+        </div>
+        <div className="flex-item__main main2">
+          <p className="about-me">
+          I moved to Montana to create and manage the content of a digital media website, with a focus on original weekly comedy podcasts.
+          We were hilarious.  As the site’s design needed tweaks, I started learning basic programming skills to do the job myself.
+          Opportunities came about that required me to put my new coding skills aside,
+          and expand my culinary skills, and eventually drove me to Portland’s food scene.
+          My love of programming never went away, and now I am back in it 100% ready to solve
+          all of Portland’s (and beyond) programming needs, on line of code at a time.
+          <br />
+          <br />
+          From website design, to app functionality, I am ready to help out wherever I can.
+          Check me out my links, or go to my contact page to get in touch.
+          {this.state.imgUrl === '/images/profile-pic.jpg'?
+          <p> BUT WHATEVER YOU DO, DON’T CLICK ON MY PICTURE!! </p> :
+          <p> ...Well, this is embarrassing.  Maybe click on it again, before people start staring.</p>
+        }
+          </p>
+        </div>
       </div>
-      <div className="flex-item__main">
-      </div>
-      <div className="flex-item__main main2">
-        <p className="about-me">
-         I am a developer with an enthusiasm for and proficiency with
-         building applications with React.  I can help with any project,
-         whether it is a simple single page application, or something
-         much larger in need of separate state containment.  I will
-         test it thoroughly, make it easy to scale, and design it so
-         that the UI is seamless and elegant.  To make that possible
-         I have worked hard to master ES6, CSS/SASS, noSQL with Firebase
-         , and version control with Git.
-        <br />
-        <br />
-        I am eager to be part of any project where I can put my skills to the test.
-        Check out my contacts page, to let me know if you would like to collaborate on
-        your next project!
-        </p>
-      </div>
-    </div>
-);
+
+    )
+  }
+};
 
 export default AboutPage;
+
+// "mailto:ebenstickney@gmail.com?subject=WeBDeV" gmail link
