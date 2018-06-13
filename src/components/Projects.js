bf6cae61507e0e54677cf45118c5a0a816710b02
ebenStickney <36087626+ebenStickney@users.noreply.github.com>
2018-06-13 14:06:19 -0700

    tweaking projects.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,27 +1,43 @@
 import React from 'react';
-import ProjectItem from './ProjectItem';
 
 
-
-
-class Projects extends React.Component {
-
-  state = {
-    modalIsOpen: false,
-  };
-
-
-  render() {
-    return (
-      <div className="box-container">
-       <div className="gallery-container">
-         <ProjectItem />
-       </div>
-      </div>
-
-    )
+const projectLinks = [
+  {
+    name: "Expensify App",
+    link: "https://expense-app-react.herokuapp.com/" ,
+    image: "/images/expensify-ss.png",
+    description: "An app that allows users to manage their fiances. Google Account Required"
+  },
+  {
+    name: "Breakout",
+    link: "https://ebenstickney.github.io/Breakout/" ,
+    image: "/images/breakout-ss.png",
+    description: "The classic Brickbreaking game"
+  },
+  {
+    name: "Indecision App",
+    link: "https://jib-indecision.herokuapp.com/" ,
+    image: "/images/indecision-ss.png",
+    description: "A single-page app that helps you decide which task to do."
   }
-};
+];
+
+const Projects = () => (
+
+  <div className="container">
+    <div className='project project__expensify'>
+      <img src="/images/expensify-ss-lg.png" alt="Expense App" />
+    </div>
+    <div className='project project__breakout'>
+      <img src="/images/breakout-ss-lg.png" alt="Breakout App" />
+    </div>
+    <div className='project project__indecision'>
+      <img src="/images/indecision-ss-lg.png" alt="Indecision App" />
+    </div>
+  </div>
+);
+
+
 
 
 
