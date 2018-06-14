8ffd0e8e954aefc608e43c84c37446b28681b107
ebenStickney <36087626+ebenStickney@users.noreply.github.com>
2018-06-14 12:58:10 -0700

    project layout
    maybe better?

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -27,12 +27,51 @@ const Projects = () => (
   <div className="container">
     <div className='project project__expensify'>
       <img src="/images/expensify-ss-lg.png" alt="Expense App" />
+      <div className="overlay-text">
+        <p className="project-description">
+          An app built to help the user keep track of and manage
+          their expenses.
+        </p>
+        <a
+          href="https://expense-app-react.herokuapp.com/"
+          className="button project-link"
+          target="_blank"
+          >
+            Check It Out
+        </a>
+      </div>
     </div>
     <div className='project project__breakout'>
       <img src="/images/breakout-ss-lg.png" alt="Breakout App" />
+      <div className="overlay-text">
+        <p className="project-description">
+          The classic brick-breaking game.
+          Created with only JS, and HTML canvas
+        </p>
+        <a
+          href="https://ebenstickney.github.io/Breakout/"
+          className="button project-link"
+          target="_blank"
+          >
+            Check It Out
+        </a>
+      </div>
     </div>
     <div className='project project__indecision'>
       <img src="/images/indecision-ss-lg.png" alt="Indecision App" />
+      <div className="overlay-text">
+        <p className="project-description">
+          A single-page app that helps the user pick a task from a
+          to-do list.
+        </p>
+        <a
+          href="https://jib-indecision.herokuapp.com/"
+          className="button project-link"
+          target="_blank"
+          >
+            Check It Out
+        </a>
+      </div>
     </div>
   </div>
 );
